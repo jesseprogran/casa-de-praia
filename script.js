1c25abd4ee49f294e1d4fd5a6cf288211359ca1b
Jessé <jessepescador22@gmail.com>
2025-01-14 21:39:23 -0300

    realizando javascript e alguns ajustes

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-document.querySelector(".menu-btn").addEventListener("click", () => {  
+document.querySelector(".menu-btn").addEventListener("click", () => {
   document.querySelector("ul.menu").classList.toggle("show");
 });
+
+function abrirReserva() {
+ 
+  window.open(
+    "https://www.airbnb.com.br/rooms/1291920922588039558?viralityEntryPoint=1&s=76",
+    "_blank"
+  );
+
+  document.getElementById("contact-form").reset();
+}
