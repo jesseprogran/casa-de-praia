f70e6e6bfbc9efc2df874c5d22ed24ebd604ad74
Jessé <jessepescador22@gmail.com>
2025-01-17 23:10:37 -0300

    realizando algumas mudanças e adcionando algumas novas seções

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-document.querySelector(".menu-btn").addEventListener("click", () => {
-  document.querySelector("ul.menu").classList.toggle("show");
+
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector("nav ul.menu");
+
+menuBtn.addEventListener("click", () => {
+  menu.classList.toggle("active");
 });
 
 function abrirReserva() {
- 
   window.open(
     "https://www.airbnb.com.br/rooms/1291920922588039558?viralityEntryPoint=1&s=76",
     "_blank"
